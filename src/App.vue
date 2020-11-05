99bd70f46952a21c98539592c01e159c39b0b382
Amy Wang <amyshuangw@gmail.com>
2020-11-05 22:02:27 +1300

    New logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Antiboredom logo" src="./assets/logo.svg">
     <Hello v-on:signalParentForClicked="onHelloClicked"/>
     <DataView/>
   </div>
