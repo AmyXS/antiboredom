2e23ec4efd7b6819abc4e17af69508bd8a6c5434
Amy Wang <amyshuangw@gmail.com>
2020-11-06 19:22:17 +1300

    Display cards in a table

--- src/components/DataView.vue
+++ src/components/DataView.vue
@@ -1,26 +1,29 @@
 <template>
   <div id="dataview">
 
-    <span v-for="(item, index) in items" :key="item.activity">
-      <b-card
-        v-bind:title=item.activity
-        v-bind:img-src=getImageURL(index)
-        img-alt="Image"
-        img-top
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text>
-          Type: {{ item.type }}
-          <br/>
-          Accessibility: {{ item.accessibility }} 
-          <br/>
-          Participants: {{ item.participants }} 
-        </b-card-text>
-      </b-card>
-    </span>
+    <b-container>
+      <b-row>
+        <span v-for="(item, index) in items" :key="item.activity">
+          <b-card
+            v-bind:title=item.activity
+            v-bind:img-src=getImageURL(index)
+            img-alt="Image"
+            img-top
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Type: {{ item.type }}
+              <br/>
+              Accessibility: {{ item.accessibility }} 
+              <br/>
+              Participants: {{ item.participants }} 
+            </b-card-text>
+          </b-card>
+        </span>
 
-      
+      </b-row>
+    </b-container>
 
     <b-button v-if="items.length" v-on:click="whatElse" >What else?</b-button>
   </div>
@@ -33,7 +36,7 @@ export default {
   props: ['items'],
   methods: {
     whatElse: function () {
-      this.$parent.getWhatToDo()
+      this.$parent.getWhatToDoTableData()
     },
     getImageURL: function (index) {
       var url ="https://placedog.net/500/280/?id=5" + index
