79c26f78a0e362ae35d5d658b2851afef9eaa087
Amy Wang <amyshuangw@gmail.com>
2020-11-08 08:48:15 +1300

    Add button styling

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="hello">
-    <b-button variant="success" v-on:click="signalParentForClicked" >What to do?</b-button>
+    <b-button id="helloBtn" variant="success" v-on:click="signalParentForClicked" >What to do?</b-button>
   </div>
 </template>
 
@@ -14,3 +14,17 @@ export default {
   }
 }
 </script>
+
+<style>
+#helloBtn {
+  background-color: #FFC20D;
+  border: none;
+  color: #241B02;
+  padding: 8px 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
