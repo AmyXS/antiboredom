a49a8fc0ea7908d7992f29cbb5094aa537a586ae
Amy Wang <amyshuangw@gmail.com>
2020-11-08 15:15:10 +1300

    Make welcome page fill height

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="hello">
-    <b-button id="helloBtn" variant="success" v-on:click="signalParentForClicked" >What to do?</b-button>
+  <div id="body">
+    <b-container>
+      <b-row class="text-center" align-v="center">
+        <b-col>
+          <div id="text" class="mb-2">
+          <p>Dear god, my mate is so bored.. </p>
+          </div>
+          <b-button id="helloBtn" variant="success" v-on:click="signalParentForClicked" >What can he do?</b-button>
+          </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
@@ -20,11 +30,18 @@ export default {
   background-color: #FFC20D;
   border: none;
   color: #241B02;
-  padding: 8px 30px;
+  padding: 10px 45px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 18px;
   font-weight: bold;
 }
+#body {
+  position: absolute;
+    top: 400px; /* Header Height */
+    bottom: 0px;
+    width: 100%;
+}
+
 </style>
